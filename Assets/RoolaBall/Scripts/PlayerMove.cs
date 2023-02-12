c45a4c370b3a28345dc4b8c7a9010b6f8fc83ac5
Netalichka <netalichka2@gmail.com>
2023-02-13 00:23:47 +0300

    playermove

---
+++ Assets/RoolaBall/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3f;
+    private Vector3 _direction;
+
+
+    private const string Horizontal = "Horizontal";
+    private const string Vertikal = "Vertical";
+
+    private void Update()
+    {
+        _direction.x = Input.GetAxis(Horizontal);
+        _direction.z = Input.GetAxis(Vertikal);
+
+        transform.position += _direction * _speed * Time.deltaTime;
+    } 
+}
