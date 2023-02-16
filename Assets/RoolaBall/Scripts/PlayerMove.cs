a9ff9366b3369a8f3868c7da174add01482f07d3
Netalichka <netalichka2@gmail.com>
2023-02-16 16:04:19 +0300

    correctmove

--- Assets/RoolaBall/Scripts/PlayerMove.cs
+++ Assets/RoolaBall/Scripts/PlayerMove.cs
@@ -10,12 +10,13 @@ public class PlayerMove : MonoBehaviour
 
     private const string Horizontal = "Horizontal";
     private const string Vertikal = "Vertical";
+    
 
     private void Update()
     {
         _direction.x = Input.GetAxis(Horizontal);
         _direction.z = Input.GetAxis(Vertikal);
-
+       
         transform.position += _direction * _speed * Time.deltaTime;
     } 
 }
