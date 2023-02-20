7e6c486924729035171a1915dc579f7da86d0602
Netalichka <netalichka2@gmail.com>
2023-02-20 19:02:21 +0300

    delegate

--- Assets/RoolaBall/Scripts/HPmanager.cs
+++ Assets/RoolaBall/Scripts/HPmanager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class HPmanager : MonoBehaviour
 {
@@ -9,16 +11,39 @@ public class HPmanager : MonoBehaviour
     [SerializeField] private float _goodBonus;
     [SerializeField] private float _badBonus;
 
+    private Rigidbody _rigidbody;
+
+    public static Action<float> HelthChange;
+
     private void Awake()
     {
         _currentHelth = _maxHelth;
     }
 
+    private void Start()
+    {
+        try
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _currentHelth += _goodBonus;
+            _currentHelth -= _badBonus;
+        }
+
+        catch
+        {
+            Debug.Log("Ошибка");
+        }
+    }
+
+    private void ReCalculateHP()
+    {
+        _currentHelth = _currentHelth + _goodBonus - _badBonus;
+        HelthChange?.Invoke(_currentHelth);
+    }
     public void Hit(float damage)
     {
         _currentHelth -= damage;
-        _currentHelth += _goodBonus;
-        _currentHelth -= _badBonus;
+       
             
         if (_currentHelth <= 0)
         {
