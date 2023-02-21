47049854e4ecff4202481af4d3593aed23e7d252
Netalichka <netalichka2@gmail.com>
2023-02-21 20:53:09 +0300

    bonuses

--- Assets/RoolaBall/Scripts/GoodBonus.cs
+++ Assets/RoolaBall/Scripts/GoodBonus.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GoodBonus : MonoBehaviour
 {
     [SerializeField] private float _goodBonus = 10f;
+
+    public void CatchBonus()
+    {
+        HPmanager.HelthChange += HelthChange;
+    }
+    
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,7 +20,10 @@ public class GoodBonus : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
     }
     // Start is called before the first frame update
 
@@ -23,4 +33,6 @@ public class GoodBonus : MonoBehaviour
     {
         
     }
+
+    HPmanager.HelthChange -= HelthChange;
 }
